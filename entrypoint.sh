66d0069d12b7ea6341b8a7411173d964558c9f30
Samuel <samueloluwade.dev@gmail.com>
2025-01-13 18:59:35 -0600

    updated azure-pipelines.yml and entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 
 if [ "$FLASK_ENV" = "production" ]; then
   echo "Running Flask in production mode..."
-  gunicorn -w 4 -b 0.0.0.0:5000 server:app
+  gunicorn -w 4 -b 0.0.0.0:5000 app:app
 else
   echo "Running Flask in development mode..."
   flask run --host=0.0.0.0 --port=5000
